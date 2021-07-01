0aefe984201b5f8631717b6a16df2e0c61000af0
RishiKallamadi <75131788+RishiKallamadi@users.noreply.github.com>
2021-07-01 09:18:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    HarryPotter = loadSound("music.mp3")
+    PeterPan = loadSound("peter_pan.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(500, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO)
+    video.hide()
+}
+
+function draw() {
+    image(video, 0, 0, 500, 400)
+}
